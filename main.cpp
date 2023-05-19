69448d59fd87b7597073145ee34adf578bf00ee6
LeCorey <125359729+LeCorey@users.noreply.github.com>
2023-05-19 19:18:39 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,57 +1,17 @@
 //
 //  main.cpp
-//  Homework 6
+//  Test
 //
-//  Created by LeCorey Johnson on 5/3/23.
+//  Created by LeCorey Johnson on 5/14/23.
 //
 
 #include <iostream>
-#include <string>
 
 int main() {
-    const int numMonths = 12;
-    std::string months[numMonths] = {"January", "February", "March", "April", "May", "June",
-                                     "July", "August", "September", "October", "November", "December"};
-    double rainfall[numMonths];
-    double totalRainfall = 0.0;
-    double averageRainfall;
-    double maxRainfall = 0.0;
-    double minRainfall = std::numeric_limits<double>::max();
-    std::string monthWithMaxRainfall, monthWithMinRainfall;
-
-    // Input validation loop
-    for (int i = 0; i < numMonths; ++i) {
-        bool validInput = false;
-        while (!validInput) {
-            std::cout << "Enter rainfall for " << months[i] << ": ";
-            if (std::cin >> rainfall[i] && rainfall[i] >= 0) {
-                validInput = true;
-            } else {
-                std::cout << "Invalid input. Rainfall cannot be negative. Please try again." << std::endl;
-                std::cin.clear();
-                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-            }
-        }
-
-        totalRainfall += rainfall[i];
-
-        if (rainfall[i] > maxRainfall) {
-            maxRainfall = rainfall[i];
-            monthWithMaxRainfall = months[i];
-        }
-
-        if (rainfall[i] < minRainfall) {
-            minRainfall = rainfall[i];
-            monthWithMinRainfall = months[i];
-        }
-    }
-
-    averageRainfall = totalRainfall / numMonths;
-
-    std::cout << "Total rainfall: " << totalRainfall << std::endl;
-    std::cout << "Average rainfall: " << averageRainfall << std::endl;
-    std::cout << "Least rainfall in: " << monthWithMinRainfall << std::endl;
-    std::cout << "Most rainfall in: " << monthWithMaxRainfall << std::endl;
-
-    return 0;
+    int x = 5;
+    if (x = 2);
+    cout<<"this is true!"<<endl;
+    else
+        
 }
+
